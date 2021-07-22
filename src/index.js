56d2102ea89f8170f9420fa5092f0d01a701e327
olhaknysh <olhaknysh1994@gmail.com>
2021-07-23 01:02:08 +0300

    add base files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from './redux/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <Provider store={store}> */}
+    <App />
+    {/* </Provider>
+      </PersistGate>
+    </BrowserRouter> */}
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+
---
+++ src/utils/routes.js
@@ -0,0 +1,9 @@
+const routes = {
+    home: '/',
+    register: '/register',
+    login: '/login',
+    calculator: '/calculator',
+    diary: '/diary'
+};
+
+export default routes;

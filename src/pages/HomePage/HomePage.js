841ae40de42a55b8f7545d3cac14d5b5dd9fcf3f
Ivanov Evhenii <ivanov191192@gmail.com>
2021-07-31 23:50:38 +0300

    fix

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,29 +6,27 @@ import Container from '../../components/common/Container';
 // import Button from '../../components/Button';
 // import DailyCalorieIntake from '../../components/DailyCalorieIntake';
 
-
-
-
 export default function HomePage() {
-
   const isWide = useMedia('(min-width: 768px)');
-  
+
   const [modal, showModal] = useState(false);
   const toggleModal = () => {
     showModal(prev => !prev);
   };
 
-
   return (
-    <> {isWide ?
-      <div className={s.bgContainer}>
-        <div className={s.background1}></div>
-        <div className={s.background2}></div>
-        <div className={s.background3}></div>
-        <div className={s.background4}></div>
-      </div> 
-      : ''
-    }
+    <>
+      {' '}
+      {isWide ? (
+        <div className={s.bgContainer}>
+          <div className={s.background1}></div>
+          <div className={s.background2}></div>
+          <div className={s.background3}></div>
+          <div className={s.background4}></div>
+        </div>
+      ) : (
+        ''
+      )}
       <Container>Page content</Container>
     </>
   );

c45e645d56130c9efb2275ceafef5ef3e742ec5f
Dmytro Vasylenko <tredstoun651@uk.net>
2021-08-01 18:07:47 +0300

    add background fixed 2

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -7,10 +7,9 @@ import FormControl from '../FormControl/FormControl';
 import ButtonAuth from '../ButtonAuth/ButtonAuth';
 import loginSchema from '../../utils/schemas/LoginSchema';
 import authOperations from '../../redux/auth/auth-operation';
-import Container from '../common/Container';
 import routes from '../../utils/routes';
 
-import styles from './LoginForm.module.css';
+import styles from './LoginForm.module.scss';
 
 const LoginForm = () => {
   const dispatch = useDispatch();

ac87fb5094bcbaac2ffd6e89c277c9409c2e7416
Dmytro Vasylenko <tredstoun651@uk.net>
2021-08-01 19:47:43 +0300

    add getCurrentUser

--- src/redux/auth/auth-reducer.js
+++ src/redux/auth/auth-reducer.js
@@ -8,6 +8,7 @@ const user = createReducer(initialUserState, {
   [authActions.registerSuccess]: (_, { payload }) => payload.user,
   [authActions.loginSuccess]: (_, { payload }) => payload.user,
   [authActions.logoutSuccess]: () => initialUserState,
+  [authActions.getCurrentUserSuccess]: (_, { payload }) => payload,
 });
 
 const token = createReducer(null, {
@@ -19,6 +20,7 @@ const error = createReducer(null, {
   [authActions.registerError]: (_, { payload }) => payload,
   [authActions.loginError]: (_, { payload }) => payload,
   [authActions.logoutError]: (_, { payload }) => payload,
+  [authActions.getCurrentUserError]: (_, { payload }) => payload,
 });
 
 const isAuthenticated = createReducer(false, {
@@ -27,6 +29,8 @@ const isAuthenticated = createReducer(false, {
   [authActions.registerError]: () => false,
   [authActions.loginError]: () => false,
   [authActions.logoutSuccess]: () => false,
+  [authActions.getCurrentUserSuccess]: () => true,
+  [authActions.getCurrentUserError]: () => false,
 });
 
 const loading = createReducer(false, {

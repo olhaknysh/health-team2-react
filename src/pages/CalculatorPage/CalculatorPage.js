2be7c4fcc05814e1e019aac0ada3018d5dcdc5ea
Ruslan Kott <ruslantisanykot@gmail.com>
2021-08-01 13:32:37 +0300

    markup CalculatorPage completed

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -2,11 +2,12 @@ import { useMedia } from 'react-use';
 import s from './CalculatorPage.module.scss';
 
 import RightSideBar from '../../components/RightSideBar';
+import CalculatorCalorieForm from '../../components/CalculatorCalorieForm'
 
 export default function CalculatorPage() {
 
-  const isWide = useMedia('(min-width: 768px)');
-  
+    const isWide = useMedia('(min-width: 480px)');
+
     return (
         <>
             {isWide ?
@@ -15,7 +16,13 @@ export default function CalculatorPage() {
                 </div>
                 : ''
             }
-            <RightSideBar />
+            <div className={s.calculatorBox}>
+                <div className={s.formBox}>
+                    <CalculatorCalorieForm />
+                </div>
+                <RightSideBar />
+
+            </div>
         </>
     )
 }

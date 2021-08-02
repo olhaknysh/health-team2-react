7bfc5180bceaaa9bccd3c931320dc9bdc13b2846
yurii-shendryk <yuriishendryk93@gmail.com>
2021-08-02 21:17:32 +0300

    update DiaryProductsListItem

--- src/components/DiaryCalendar/DiaryProductsListItem/DiaryProductsListItem.js
+++ src/components/DiaryCalendar/DiaryProductsListItem/DiaryProductsListItem.js
@@ -1,21 +1,33 @@
 import React from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './DiaryProductsListItem.module.scss';
+
+import { productsOperations, productsSelectors } from '../../../redux/products';
 
 // Icon
 import icon from '../../../utils/images/diary-delete-icon.svg';
 
-const DiaryProductsListItem = () => {
-  const handleDelete = () => {
-    console.log('delete');
+const DiaryProductsListItem = ({ id }) => {
+  const dispatch = useDispatch();
+  const { title, weight, calories } = useSelector(
+    productsSelectors.getProduct(id),
+  );
+  console.log(title);
+  const handleDelete = event => {
+    event.preventDefault();
+    try {
+      dispatch(productsOperations.deleteProducts(id));
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
     <li className={styles.item}>
-      <p className={styles.product}>Баклажан</p>
-      <p className={styles.quantity}>100 г</p>
+      <p className={styles.product}>{title}</p>
+      <p className={styles.quantity}>{weight} г</p>
       <p className={styles.calories}>
-        320 <span className={styles.cal}>ккал</span>
+        {calories} <span className={styles.cal}>ккал</span>
       </p>
       <button type="button" className={styles.delete} onClick={handleDelete}>
         <img src={icon} alt="delete icon" />

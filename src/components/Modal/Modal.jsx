dd8cbbdd8f3b9573c9671de8d540f18816280379
Alex <js.fullstack.development@gmail.com>
2021-08-03 03:02:57 +0300

    Fixed modal, add animation is open modal and component CaloriesIntake

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,8 @@
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.scss';
-import { CSSTransition } from 'react-transition-group';
 const modalRoot = document.getElementById('root-modal');
 const Modal = ({ onClose, children }) => {
-  console.log(onClose);
   useEffect(() => {
     const handlekeyDown = e => {
       if (e.code === 'Escape') {
@@ -22,11 +20,9 @@ const Modal = ({ onClose, children }) => {
   };
 
   return createPortal(
-    <CSSTransition in={onClose} timeout={5000}>
-      <div onClick={handleBackdropClick} className={styles.Overlay}>
-        <div className={styles.Modal}>{children}</div>
-      </div>
-    </CSSTransition>,
+    <div onClick={handleBackdropClick} className={styles.Overlay}>
+      <div className={styles.Modal}>{children}</div>
+    </div>,
     modalRoot,
   );
 };

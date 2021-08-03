c106028eb3cc3e9443e1f46a134b59926954407c
olhaknysh <olhaknysh1994@gmail.com>
2021-08-03 14:53:48 +0300

    render lists products on date

--- src/components/DiaryCalendar/DiaryAddProductForm/DiaryAddProductForm.js
+++ src/components/DiaryCalendar/DiaryAddProductForm/DiaryAddProductForm.js
@@ -13,110 +13,115 @@ import { productsOperations } from '../../../redux/products';
 axios.defaults.baseURL = 'https://slim-mom-app.herokuapp.com/api';
 
 const DiaryAddProductForm = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [title, setTitle] = useState('');
-  const [weight, setWeight] = useState('');
-  const [query, setQuery] = useState('');
-  const [productsState, setProductsState] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [title, setTitle] = useState('');
+    const [weight, setWeight] = useState('');
+    const [query, setQuery] = useState('');
+    const [productsState, setProductsState] = useState([]);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const date = useSelector(calendarSelectors.currentDate);
-  // const isLoading = useSelector(calendarSelectors.isLoading);
+    const date = useSelector(calendarSelectors.currentDate);
+    // const isLoading = useSelector(calendarSelectors.isLoading);
 
-  const fetchProductsData = async value => {
-    try {
-      const { data } = await axios.get(`/products?search=${value}`);
-      setProductsState(data.products);
-    } catch (e) {}
-  };
+    const fetchProductsData = async value => {
+        try {
+            const { data } = await axios.get(`/products?search=${value}`);
+            setProductsState(data.products);
+        } catch (e) { }
+    };
 
-  useEffect(() => {
-    if (!query) {
-      setIsOpen(false);
-      return;
+    useEffect(() => {
+        if (!query) {
+            setIsOpen(false);
+            return;
+        }
+        fetchProductsData(query);
+    }, [query]);
+
+    const handleAddProducts = async (values) => {
+        await dispatch(productsOperations.addProducts(values));
+        await dispatch(productsOperations.getProductsByDay(date));
     }
-    fetchProductsData(query);
-  }, [query]);
 
-  const handleSubmit = async (event, values) => {
-    event.preventDefault();
+    const handleSubmit = async (event, values) => {
+        event.preventDefault();
 
-    try {
-      dispatch(productsOperations.addProducts(values));
-      setTitle('');
-      setWeight('');
-      setIsOpen(false);
-    } catch (e) {}
-  };
+        try {
+            handleAddProducts(values);
+            setTitle('');
+            setWeight('');
+            setIsOpen(false);
+        } catch (e) { }
+    };
 
-  const handleTitleChange = e => {
-    setTitle(e.target.value);
-    setQuery(e.target.value);
-    setIsOpen(true);
-  };
+    const handleTitleChange = e => {
+        setTitle(e.target.value);
+        setQuery(e.target.value);
+        setIsOpen(true);
+    };
 
-  return (
-    <React.Fragment>
-      <form
-        className={styles.form}
-        onSubmit={e => handleSubmit(e, { title, weight, date })}
-      >
-        <div>
-          <input
-            className={styles.input}
-            label="Введите название продукта"
-            placeholder="Введите название продукта *"
-            id="title"
-            name="title"
-            type="text"
-            value={title}
-            required
-            autoComplete="off"
-            onChange={e => handleTitleChange(e)}
-            onBlur={() => {
-              setTimeout(() => {
-                setIsOpen(false);
-              }, 300);
-            }}
-          />
-          {isOpen && (
-            <ul className={styles.productList}>
-              {productsState.map(el => {
-                return (
-                  <li
-                    key={el.title.ru}
-                    onClick={() => {
-                      setTitle(el.title.ru);
-                      setIsOpen(false);
-                    }}
-                  >
-                    <span>{el.title.ru}</span>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
+    return (
+        <React.Fragment>
+            <form
+                className={styles.form}
+                onSubmit={e => handleSubmit(e, { title, weight, date })}
+            >
+                <div>
+                    <input
+                        className={styles.input}
+                        label="Введите название продукта"
+                        placeholder="Введите название продукта *"
+                        id="title"
+                        name="title"
+                        type="text"
+                        value={title}
+                        required
+                        autoComplete="off"
+                        onChange={e => handleTitleChange(e)}
+                        onBlur={() => {
+                            setTimeout(() => {
+                                setIsOpen(false);
+                            }, 300);
+                        }}
+                    />
+                    {isOpen && (
+                        <ul className={styles.productList}>
+                            {productsState.map(el => {
+                                return (
+                                    <li
+                                        key={el.title.ru}
+                                        onClick={() => {
+                                            setTitle(el.title.ru);
+                                            setIsOpen(false);
+                                        }}
+                                    >
+                                        <span>{el.title.ru}</span>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    )}
+                </div>
 
-        <input
-          className={styles.input}
-          label="Граммы"
-          placeholder="Граммы *"
-          id="weight"
-          name="weight"
-          type="number"
-          required
-          value={weight}
-          onChange={e => setWeight(e.target.value)}
-        />
+                <input
+                    className={styles.input}
+                    label="Граммы"
+                    placeholder="Граммы *"
+                    id="weight"
+                    name="weight"
+                    type="number"
+                    required
+                    value={weight}
+                    onChange={e => setWeight(e.target.value)}
+                />
 
-        <button type="submit" className={styles.button}>
-          <img src={icon} alt="form plus icon" />
-        </button>
-      </form>
-    </React.Fragment>
-  );
+                <button type="submit" className={styles.button}>
+                    <img src={icon} alt="form plus icon" />
+                </button>
+            </form>
+        </React.Fragment>
+    );
 };
 
 export default DiaryAddProductForm;
--- src/components/DiaryCalendar/DiaryProductsList/DiaryProductsList.js
+++ src/components/DiaryCalendar/DiaryProductsList/DiaryProductsList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './DiaryProductsList.module.scss';
 import { useSelector } from 'react-redux';
@@ -7,25 +7,28 @@ import { productsSelectors } from '../../../redux/products';
 import DiaryProductsListItem from '../DiaryProductsListItem';
 
 const DiaryProductsList = () => {
-  const products = useSelector(productsSelectors.getProducts);
-  // const productsByDay = useSelector(productsSelectors.getProductsByDay);
-  // const isLoading = useSelector(productsSelectors.isLoading);
+    const productsByDay = useSelector(productsSelectors.getProductsByDay);
+    const products = useSelector(productsSelectors.getProducts)
+    // const isLoading = useSelector(productsSelectors.isLoading);
+    console.log(productsByDay);
 
-  return (
-    <div className={styles.container}>
-      {products.length === 0 && (
-        <h2 className={styles.noProductsHeading}>
-          Добавте продукты за сегодня
-        </h2>
-      )}
-      <ul className={styles.list}>
-        {products.map(({ _id }) => (
-          <DiaryProductsListItem key={_id} id={_id} />
-        ))}
-      </ul>
-      <div className={styles.overlay}></div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            {productsByDay?.length === 0 && (
+                <h2 className={styles.noProductsHeading}>
+                    Добавте продукты за сегодня
+                </h2>
+            )}
+            <ul className={styles.list}>
+                {productsByDay?.map(({ _id, title, weight, calories }) => (
+                    <DiaryProductsListItem key={_id} id={_id} title={title} weight={weight} calories={calories} />
+                ))}
+            </ul>
+
+
+            <div className={styles.overlay}></div>
+        </div>
+    );
 };
 
 export default DiaryProductsList;

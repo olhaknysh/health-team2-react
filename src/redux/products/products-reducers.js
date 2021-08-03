1dee9a965a8b2c710cb68573900f44ad42332b9f
olhaknysh <olhaknysh1994@gmail.com>
2021-08-04 00:03:24 +0300

    delete fix

--- src/redux/products/products-reducers.js
+++ src/redux/products/products-reducers.js
@@ -6,18 +6,21 @@ import productsActions from './products-actions';
 const dailyNormalProcent = createReducer(0, {
     [productsActions.getProductsByDaySuccess]: (_, { payload: { dailyNormalProcent } }) => dailyNormalProcent,
     [productsActions.addProductsOnDaySuccess]: (_, { payload: { dailyNormalProcent } }) => dailyNormalProcent,
+    [productsActions.deleteProductsOnDaySuccess]: (_, { payload: { data: { dailyNormalProcent } } }) => dailyNormalProcent,
     [productsActions.getProductsByDayError]: () => 0
 })
 
 const leftCalories = createReducer(0, {
     [productsActions.getProductsByDaySuccess]: (_, { payload: { leftCalories } }) => leftCalories,
     [productsActions.addProductsOnDaySuccess]: (_, { payload: { leftCalories } }) => leftCalories,
+    [productsActions.deleteProductsOnDaySuccess]: (_, { payload: { data: { leftCalories } } }) => leftCalories,
     [productsActions.getProductsByDayError]: () => 0
 })
 
 const totalCalories = createReducer(0, {
     [productsActions.getProductsByDaySuccess]: (_, { payload: { totalCalories } }) => totalCalories,
     [productsActions.addProductsOnDaySuccess]: (_, { payload: { totalCalories } }) => totalCalories,
+    [productsActions.deleteProductsOnDaySuccess]: (_, { payload: { data: { totalCalories } } }) => totalCalories,
     [productsActions.getProductsByDayError]: () => 0
 })
 
@@ -28,8 +31,8 @@ const productsByDay = createReducer([], {
         ...state,
         product,
     ],
-    [productsActions.deleteProductsOnDaySuccess]: (state, { payload }) =>
-        state.filter(({ _id }) => _id !== payload),
+    [productsActions.deleteProductsOnDaySuccess]: (state, { payload: { productId } }) =>
+        state.filter(({ _id }) => _id !== productId),
 });
 
 const error = createReducer(null, {

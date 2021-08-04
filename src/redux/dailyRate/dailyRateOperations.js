7eaf80df50b8da16635f272a3aad1e51b0c0b05a
yurii-shendryk <yuriishendryk93@gmail.com>
2021-08-04 22:32:19 +0300

    update error message

--- src/redux/dailyRate/dailyRateOperations.js
+++ src/redux/dailyRate/dailyRateOperations.js
@@ -5,36 +5,36 @@ import { toast } from 'react-toastify';
 axios.defaults.baseURL = 'https://slim-mom-app.herokuapp.com/api';
 
 const onFetchDailyRates = values => dispatch => {
-    dispatch(dailyRateActions.fetchDailyRateRequest());
+  dispatch(dailyRateActions.fetchDailyRateRequest());
 
-    axios
-        .post('/users/calories', values)
-        .then(receivedData => {
-            dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data));
-        })
-        .catch(error => {
-            toast.error(error.message);
-            dispatch(dailyRateActions.fetchDailyRateError(error.message));
-        });
+  axios
+    .post('/users/calories', values)
+    .then(receivedData => {
+      dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data));
+    })
+    .catch(error => {
+      toast.error(error.message);
+      dispatch(dailyRateActions.fetchDailyRateError(error.message));
+    });
 };
 
 const onFetchDailyRatesAuthorised =
-    (values, userId) => (dispatch, getState) => {
-        dispatch(dailyRateActions.fetchDailyRateRequestAuth());
+  (values, userId) => (dispatch, getState) => {
+    dispatch(dailyRateActions.fetchDailyRateRequestAuth());
 
-        axios
-            .post(`/users/${userId}/calories`, values)
-            .then(receivedData => {
-                dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data));
-            })
-            .catch(error => {
-                toast.error(error.message);
-                dispatch(dailyRateActions.fetchDailyRateErrorAuth(error.message));
-            });
-    };
+    axios
+      .post(`/users/${userId}/calories`, values)
+      .then(receivedData => {
+        dispatch(dailyRateActions.fetchDailyRateSuccess(receivedData.data));
+      })
+      .catch(error => {
+        toast.error(error.message);
+        dispatch(dailyRateActions.fetchDailyRateErrorAuth(error.message));
+      });
+  };
 
 // eslint-disable-next-line
 export default {
-    onFetchDailyRates,
-    onFetchDailyRatesAuthorised,
+  onFetchDailyRates,
+  onFetchDailyRatesAuthorised,
 };

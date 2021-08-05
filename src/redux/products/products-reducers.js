9e780e5d5bb04246d45ce6e09744e50c93cc9558
olhaknysh <olhaknysh1994@gmail.com>
2021-08-05 09:51:17 +0300

    fix mentor comments

--- src/redux/products/products-reducers.js
+++ src/redux/products/products-reducers.js
@@ -1,27 +1,31 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
 import productsActions from './products-actions';
+import authActions from '../auth/auth-actions';
 
 
 const dailyNormalProcent = createReducer(0, {
     [productsActions.getProductsByDaySuccess]: (_, { payload: { dailyNormalProcent } }) => dailyNormalProcent,
     [productsActions.addProductsOnDaySuccess]: (_, { payload: { dailyNormalProcent } }) => dailyNormalProcent,
     [productsActions.deleteProductsOnDaySuccess]: (_, { payload: { data: { dailyNormalProcent } } }) => dailyNormalProcent,
-    [productsActions.getProductsByDayError]: () => 0
+    [productsActions.getProductsByDayError]: () => 0,
+    [authActions.logoutSuccess]: () => 0
 })
 
 const leftCalories = createReducer(0, {
     [productsActions.getProductsByDaySuccess]: (_, { payload: { leftCalories } }) => leftCalories,
     [productsActions.addProductsOnDaySuccess]: (_, { payload: { leftCalories } }) => leftCalories,
     [productsActions.deleteProductsOnDaySuccess]: (_, { payload: { data: { leftCalories } } }) => leftCalories,
-    [productsActions.getProductsByDayError]: () => 0
+    [productsActions.getProductsByDayError]: () => 0,
+    [authActions.logoutSuccess]: () => 0
 })
 
 const totalCalories = createReducer(0, {
     [productsActions.getProductsByDaySuccess]: (_, { payload: { totalCalories } }) => totalCalories,
     [productsActions.addProductsOnDaySuccess]: (_, { payload: { totalCalories } }) => totalCalories,
     [productsActions.deleteProductsOnDaySuccess]: (_, { payload: { data: { totalCalories } } }) => totalCalories,
-    [productsActions.getProductsByDayError]: () => 0
+    [productsActions.getProductsByDayError]: () => 0,
+    [authActions.logoutSuccess]: () => 0
 })
 
 const productsByDay = createReducer([], {
@@ -33,14 +37,23 @@ const productsByDay = createReducer([], {
     ],
     [productsActions.deleteProductsOnDaySuccess]: (state, { payload: { productId } }) =>
         state.filter(({ _id }) => _id !== productId),
+    [authActions.logoutSuccess]: () => []
 });
 
 const error = createReducer(null, {
+    [productsActions.getProductsByDayError]: (_, { payload }) => payload,
     [productsActions.addProductsOnDayError]: (_, { payload }) => payload,
     [productsActions.deleteProductsOnDayError]: (_, { payload }) => payload,
+    [productsActions.getProductsByDaySuccess]: () => null,
+    [productsActions.addProductsOnDaySuccess]: () => null,
+    [productsActions.deleteProductsOnDaySuccess]: () => null,
+    [authActions.logoutSuccess]: () => null
 });
 
 const loading = createReducer(false, {
+    [productsActions.getProductsByDayRequest]: () => true,
+    [productsActions.getProductsByDaySuccess]: () => false,
+    [productsActions.getProductsByDayError]: () => false,
     [productsActions.addProductsOnDayRequest]: () => true,
     [productsActions.addProductsOnDaySuccess]: () => false,
     [productsActions.addProductsOnDayError]: () => false,
